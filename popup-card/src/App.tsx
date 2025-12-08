b7857497092aac8d92cf9773c3077cfd32c7f1db
asgharali97 <asgharalifs48@gmail.com>
2025-12-09 00:13:34 +0500

    sales dashboard component tried

--- popup-card/src/App.tsx
+++ popup-card/src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import PopupCard from "./components/PopupCard";
-import PopupCardGsap from "./components/PopupCardGsap";
+import DashboardCard from "./components/DashboardCard";
 const App = () => {
   return (
     <>
       <div className="w-full h-screen bg-neutral-100 text-black flex justify-center items-center geist">
-        <div className="w-92 82  flex justify-center rouned-lg mx-4 py-4">
-          <PopupCard />
-        </div>
-        <div className="w-92 82  flex justify-center rouned-lg mx-4 py-4">
-          <PopupCardGsap />
-        </div>
+        <DashboardCard/>
       </div>
     </>
   );
